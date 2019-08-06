9f1442fcf66afeb3a5f09ea1ca36210c40dbbb2f
takenoko-str <taku.hyogo.kei@gmail.com>
2019-08-06 13:46:30 +0900

    fix: 識別子の変更

--- examples/network.tf
+++ examples/network.tf
@@ -1,159 +1,159 @@
-resource "aws_vpc" "vpc-stg-hoge" {
+resource "aws_vpc" "vpc-identifier" {
     cidr_block = "10.129.0.0/16"
     enable_dns_hostnames = true
     tags {
-        Name = "vpc-stg-hoge"
+        Name = "vpc-identifier"
     }
 }
 
-resource "aws_internet_gateway" "igw-stg-hoge" {
-  vpc_id = "${aws_vpc.vpc-stg-hoge.id}"
+resource "aws_internet_gateway" "igw-identifier" {
+  vpc_id = "${aws_vpc.vpc-identifier.id}"
 
   tags {
-    Name = "igw-stg-hoge"
+    Name = "igw-identifier"
   }
 }
 
-resource "aws_route_table" "rtb-stg-hoge-elb" {
-  vpc_id = "${aws_vpc.vpc-stg-hoge.id}"
+resource "aws_route_table" "rtb-identifier-elb" {
+  vpc_id = "${aws_vpc.vpc-identifier.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.igw-stg-hoge.id}"
+    gateway_id = "${aws_internet_gateway.igw-identifier.id}"
   }
 
   tags {
-    Name = "rtb-stg-hoge-elb"
+    Name = "rtb-identifier-elb"
   }
 }
 
-resource "aws_route_table" "rtb-stg-hoge-ap" {
-  vpc_id = "${aws_vpc.vpc-stg-hoge.id}"
+resource "aws_route_table" "rtb-identifier-ap" {
+  vpc_id = "${aws_vpc.vpc-identifier.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_nat_gateway.nat-stg-hoge.id}"
+    gateway_id = "${aws_nat_gateway.nat-identifier.id}"
   }
 
   tags {
-    Name = "rtb-stg-hoge-ap"
+    Name = "rtb-identifier-ap"
   }
 }
 
-resource "aws_route_table" "rtb-stg-hoge-db" {
-  vpc_id = "${aws_vpc.vpc-stg-hoge.id}"
+resource "aws_route_table" "rtb-identifier-db" {
+  vpc_id = "${aws_vpc.vpc-identifier.id}"
 
   tags {
-    Name = "rtb-stg-hoge-db"
+    Name = "rtb-identifier-db"
   }
 }
 
-resource "aws_subnet" "subnet-stg-hoge-elb-a" {
-  vpc_id     = "${aws_vpc.vpc-stg-hoge.id}"
+resource "aws_subnet" "subnet-identifier-elb-a" {
+  vpc_id     = "${aws_vpc.vpc-identifier.id}"
   cidr_block = "10.129.0.0/24"
   map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1a"
   tags {
-    Name = "subnet-stg-hoge-elb-a"
+    Name = "subnet-identifier-elb-a"
   }
-  depends_on = ["aws_internet_gateway.igw-stg-hoge"]
+  depends_on = ["aws_internet_gateway.igw-identifier"]
 }
 
-resource "aws_route_table_association" "rtbassoc-stg-hoge-elb-a" {
-  subnet_id      = "${aws_subnet.subnet-stg-hoge-elb-a.id}"
-  route_table_id = "${aws_route_table.rtb-stg-hoge-elb.id}"
+resource "aws_route_table_association" "rtbassoc-identifier-elb-a" {
+  subnet_id      = "${aws_subnet.subnet-identifier-elb-a.id}"
+  route_table_id = "${aws_route_table.rtb-identifier-elb.id}"
 }
 
-resource "aws_subnet" "subnet-stg-hoge-elb-c" {
-  vpc_id     = "${aws_vpc.vpc-stg-hoge.id}"
+resource "aws_subnet" "subnet-identifier-elb-c" {
+  vpc_id     = "${aws_vpc.vpc-identifier.id}"
   cidr_block = "10.129.1.0/24"
   map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1c"
   tags {
-    Name = "subnet-stg-hoge-elb-c"
+    Name = "subnet-identifier-elb-c"
   }
-  depends_on = ["aws_internet_gateway.igw-stg-hoge"]
+  depends_on = ["aws_internet_gateway.igw-identifier"]
 }
 
-resource "aws_route_table_association" "rtbassoc-stg-hoge-elb-c" {
-  subnet_id      = "${aws_subnet.subnet-stg-hoge-elb-c.id}"
-  route_table_id = "${aws_route_table.rtb-stg-hoge-elb.id}"
+resource "aws_route_table_association" "rtbassoc-identifier-elb-c" {
+  subnet_id      = "${aws_subnet.subnet-identifier-elb-c.id}"
+  route_table_id = "${aws_route_table.rtb-identifier-elb.id}"
 }
 
-resource "aws_subnet" "subnet-stg-hoge-ap-a" {
-  vpc_id     = "${aws_vpc.vpc-stg-hoge.id}"
+resource "aws_subnet" "subnet-identifier-ap-a" {
+  vpc_id     = "${aws_vpc.vpc-identifier.id}"
   cidr_block = "10.129.16.0/24"
   map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1a"
   tags {
-    Name = "subnet-stg-hoge-ap-a"
+    Name = "subnet-identifier-ap-a"
   }
-  depends_on = ["aws_internet_gateway.igw-stg-hoge"]
+  depends_on = ["aws_internet_gateway.igw-identifier"]
 }
 
-resource "aws_route_table_association" "rtbassoc-stg-hoge-ap-a" {
-  subnet_id      = "${aws_subnet.subnet-stg-hoge-ap-a.id}"
-  route_table_id = "${aws_route_table.rtb-stg-hoge-ap.id}"
+resource "aws_route_table_association" "rtbassoc-identifier-ap-a" {
+  subnet_id      = "${aws_subnet.subnet-identifier-ap-a.id}"
+  route_table_id = "${aws_route_table.rtb-identifier-ap.id}"
 }
 
-resource "aws_subnet" "subnet-stg-hoge-ap-c" {
-  vpc_id     = "${aws_vpc.vpc-stg-hoge.id}"
+resource "aws_subnet" "subnet-identifier-ap-c" {
+  vpc_id     = "${aws_vpc.vpc-identifier.id}"
   cidr_block = "10.129.17.0/24"
   map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1c"
   tags {
-    Name = "subnet-stg-hoge-ap-c"
+    Name = "subnet-identifier-ap-c"
   }
-  depends_on = ["aws_internet_gateway.igw-stg-hoge"]
+  depends_on = ["aws_internet_gateway.igw-identifier"]
 }
 
-resource "aws_route_table_association" "rtbassoc-stg-hoge-ap-c" {
-  subnet_id      = "${aws_subnet.subnet-stg-hoge-ap-c.id}"
-  route_table_id = "${aws_route_table.rtb-stg-hoge-ap.id}"
+resource "aws_route_table_association" "rtbassoc-identifier-ap-c" {
+  subnet_id      = "${aws_subnet.subnet-identifier-ap-c.id}"
+  route_table_id = "${aws_route_table.rtb-identifier-ap.id}"
 }
 
-resource "aws_subnet" "subnet-stg-hoge-db-a" {
-  vpc_id     = "${aws_vpc.vpc-stg-hoge.id}"
+resource "aws_subnet" "subnet-identifier-db-a" {
+  vpc_id     = "${aws_vpc.vpc-identifier.id}"
   cidr_block = "10.129.56.0/24"
   map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1a"
   tags {
-    Name = "subnet-stg-hoge-db-a"
+    Name = "subnet-identifier-db-a"
   }
-  depends_on = ["aws_internet_gateway.igw-stg-hoge"]
+  depends_on = ["aws_internet_gateway.igw-identifier"]
 }
 
-resource "aws_route_table_association" "rtbassoc-stg-hoge-db-a" {
-  subnet_id      = "${aws_subnet.subnet-stg-hoge-db-a.id}"
-  route_table_id = "${aws_route_table.rtb-stg-hoge-db.id}"
+resource "aws_route_table_association" "rtbassoc-identifier-db-a" {
+  subnet_id      = "${aws_subnet.subnet-identifier-db-a.id}"
+  route_table_id = "${aws_route_table.rtb-identifier-db.id}"
 }
 
-resource "aws_subnet" "subnet-stg-hoge-db-c" {
-  vpc_id     = "${aws_vpc.vpc-stg-hoge.id}"
+resource "aws_subnet" "subnet-identifier-db-c" {
+  vpc_id     = "${aws_vpc.vpc-identifier.id}"
   cidr_block = "10.129.57.0/24"
   map_public_ip_on_launch = true
   availability_zone = "ap-northeast-1c"
   tags {
-    Name = "subnet-stg-hoge-db-c"
+    Name = "subnet-identifier-db-c"
   }
-  depends_on = ["aws_internet_gateway.igw-stg-hoge"]
+  depends_on = ["aws_internet_gateway.igw-identifier"]
 }
 
-resource "aws_route_table_association" "rtbassoc-stg-hoge-db-c" {
-  subnet_id      = "${aws_subnet.subnet-stg-hoge-db-c.id}"
-  route_table_id = "${aws_route_table.rtb-stg-hoge-db.id}"
+resource "aws_route_table_association" "rtbassoc-identifier-db-c" {
+  subnet_id      = "${aws_subnet.subnet-identifier-db-c.id}"
+  route_table_id = "${aws_route_table.rtb-identifier-db.id}"
 }
 
-resource "aws_eip" "eip-stg-hoge-nat" {
+resource "aws_eip" "eip-identifier-nat" {
   vpc = true
-  depends_on = ["aws_internet_gateway.igw-stg-hoge"]
+  depends_on = ["aws_internet_gateway.igw-identifier"]
 }
 
-resource "aws_nat_gateway" "nat-stg-hoge" {
-  allocation_id = "${aws_eip.eip-stg-hoge-nat.id}"
-  subnet_id     = "${aws_subnet.subnet-stg-hoge-elb-a.id}"
+resource "aws_nat_gateway" "nat-identifier" {
+  allocation_id = "${aws_eip.eip-identifier-nat.id}"
+  subnet_id     = "${aws_subnet.subnet-identifier-elb-a.id}"
   tags {
-    Name = "eip-stg-hoge-nat"
+    Name = "eip-identifier-nat"
   }
-  depends_on = ["aws_internet_gateway.igw-stg-hoge"]
+  depends_on = ["aws_internet_gateway.igw-identifier"]
 }
