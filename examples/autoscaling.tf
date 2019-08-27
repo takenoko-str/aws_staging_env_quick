3854229510d1ff75c9f1ec4073f4506a9088423a
takenoko-str <taku.hyogo.kei@gmail.com>
2019-08-27 18:34:16 +0900

    feat: ライフサイクル、ポリシー、通知

--- examples/autoscaling.tf
+++ examples/autoscaling.tf
@@ -4,6 +4,7 @@ variable "iam_instance_profile" {}
 variable "aws_account_number" {}
 variable "target_group_arn" {}
 variable "sg_identifier_ap" {}
+variable "sns_topic_arn" {}
 variable "instance_type" {}
 variable "ami_name" {}
 variable "key_name" {}
@@ -85,4 +86,82 @@ resource "aws_autoscaling_group" "as-identifier" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_sqs_queue" "graceful_termination_queue" {
+  name = "graceful_termination_queue"
+}
+
+resource "aws_iam_role" "autoscaling_role" {
+  name = "autoscaling_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "autoscaling.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lifecycle_hook_autoscaling_policy" {
+  name = "lifecycle_hook_autoscaling_policy"
+  role = "${aws_iam_role.autoscaling_role.id}"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "Stmt1436380187000",
+            "Effect": "Allow",
+            "Action": [
+                "sqs:GetQueueUrl",
+                "sqs:SendMessage"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_autoscaling_lifecycle_hook" "asg_hook_identifier" {
+  name = "asg_hook_identifier"
+  autoscaling_group_name  = "${aws_autoscaling_group.as-identifier.name}"
+  notification_target_arn = "${aws_sqs_queue.graceful_termination_queue.arn}"
+  role_arn = "${aws_iam_role.autoscaling_role.arn}"
+  default_result          = "ABANDON"
+  heartbeat_timeout       = 300
+  lifecycle_transition    = "autoscaling:EC2_INSTANCE_TERMINATING"
+}
+
+resource "aws_autoscaling_policy" "asg_policy_identifier" {
+  name                   = "asg_policy_identifier"
+  scaling_adjustment     = 4
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = "${aws_autoscaling_group.as-identifier.name}"
+}
+
+resource "aws_autoscaling_notification" "asg_notification" {
+  group_names = [
+    "${aws_autoscaling_group.as-identifier.name}"
+  ]
+
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR",
+    "autoscaling:EC2_INSTANCE_TERMINATE_ERROR",
+  ]
+  topic_arn = "${var.sns_topic_arn}"
 }
