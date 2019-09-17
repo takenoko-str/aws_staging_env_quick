0e82e0101bc9a49d0fe6367cdfb481da21c26c41
Takuya Saito <takuya@inside.ai>
2019-09-17 19:42:04 +0900

    fix: validateエラー修正

--- examples/network/boot.sh
+++ examples/network/boot.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+AWS_S3_KEY=.terraform/network/terraform.tfstate
+source .env
+
 terraform init \
     -backend-config "bucket=$AWS_S3_BUCKET" \
     -backend-config "key=$AWS_S3_KEY"
+
+terraform plan
+
