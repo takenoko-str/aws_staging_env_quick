f9b2048a92e956be2b967079ac71579217938db6
Takuya Saito <takuya@inside.ai>
2019-09-20 18:47:54 +0900

    feat: security group

---
+++ examples/firewall/main.tf
@@ -0,0 +1,41 @@
+variable "s3_bucket_name" {}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket = "${var.s3_bucket_name}"
+    key    = ".terraform/network/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_security_group" "sg-identifier-lb" {
+  name        = "identifier-lb"
+  description = "Allow all https inbound traffic"
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+  tags = {
+    Name = "identifier-lb"
+  }
+}
+
+resource "aws_security_group_rule" "sg-identifier-lb-https-ingress" {
+    type              = "ingress"
+    from_port         = 443
+    to_port           = 443
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.sg-identifier-lb.id
+}
+
+resource "aws_security_group_rule" "sg-identifier-all-egress" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg-identifier-lb.id
+}
