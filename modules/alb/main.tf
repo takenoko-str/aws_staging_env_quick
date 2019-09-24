c1a0c138829c4a3f7ccddab11f001ee3a5eb3efc
takenoko-str <taku.hyogo.kei@gmail.com>
2019-09-24 14:33:10 +0900

    style: ファイルを分割

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,9 +1,3 @@
-variable "acm_yourdomain" {}
-variable "subnet_identifier_lb_a" {}
-variable "subnet_identifier_lb_c" {}
-variable "sg_identifier_lb" {}
-variable "vpc_identifier" {}
-
 data "aws_acm_certificate" "acm_yourdomain" {
   types  = ["AMAZON_ISSUED"]
   domain = "${var.acm_yourdomain}"
@@ -76,4 +70,31 @@ resource "aws_lb_listener" "ln-identifier" {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.lb-tg-identifier.arn}"
   }
+}
+
+resource "aws_security_group" "sg-identifier-lb" {
+  name        = "identifier-lb"
+  description = "Allow all https inbound traffic"
+  vpc_id      = data.terraform_remote_state.network.outputs.vpc_id
+  tags = {
+    Name = "identifier-lb"
+  }
+}
+
+resource "aws_security_group_rule" "sg-identifier-lb-https-ingress" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg-identifier-lb.id
+}
+
+resource "aws_security_group_rule" "sg-identifier-all-egress" {
+  type              = "egress"
+  to_port           = 0
+  protocol          = "-1"
+  from_port         = 0
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg-identifier-lb.id
 }
